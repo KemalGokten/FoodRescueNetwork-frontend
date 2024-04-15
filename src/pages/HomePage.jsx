0be19dacc19388955440a78e0bd29f71ec2f16f7
Saeideh Bahadoran <saeideh.bahadoran@gmail.com>
2024-04-15 18:03:50 +0200

    Add restaurantcomponents to Home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import Navbar from "../components/Navbar/Navbar";
+import AllRestaurants from "../components/HomePage/AllRestaurants";
+import HighlyRecommendedRestaurants from "../components/HomePage/HighlyRecommendedRestaurants";
+import RandomChoices from "../components/HomePage/RandomChoices";
 const HomePage = () => {
   return (
     <>
       <Navbar />
-      <h1>Home Page</h1>;
+      <h1>Home Page</h1>
+      <HighlyRecommendedRestaurants />
+      <RandomChoices />
+      <AllRestaurants />
     </>
   );
 };
