8b7022f89bbc94e63ac7eb7194c66e670888b79d
Saeideh Bahadoran <saeideh.bahadoran@gmail.com>
2024-04-15 18:00:33 +0200

    Import Carousel style Css

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { MantineProvider } from '@mantine/core'
-import App from './App.jsx'
-import {BrowserRouter as Router} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import '@mantine/core/styles.css'
-import './styles/global.css'
-import theme from './styles/theme.js'
-import { AuthProvider } from './contexts/AuthContext.jsx';
+import "@mantine/core/styles.css";
+import "@mantine/carousel/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./styles/global.css";
+import theme from "./styles/theme.js";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MantineProvider theme={theme}>
-    <AuthProvider>
-      <Router>
-        <App />
-      </Router>
-    </AuthProvider>
+      <AuthProvider>
+        <Router>
+          <App />
+        </Router>
+      </AuthProvider>
     </MantineProvider>
   </React.StrictMode>
-)
+);
