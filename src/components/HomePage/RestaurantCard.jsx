62c4b2a070eb5191b2d45dd7fac8b21b4145c6b8
KemalGokten <ukemalgokten@gmail.com>
2024-04-16 19:00:26 +0200

    Show all restaurants and add sortBy and filterBy functionality

--- src/components/HomePage/RestaurantCard.jsx
+++ src/components/HomePage/RestaurantCard.jsx
@@ -1,26 +1,22 @@
 import { Card, Image, Text } from "@mantine/core";
+import { Link } from "react-router-dom";
 
 const RestaurantCard = ({ restaurantData }) => {
   return (
-    <Card
-      shadow="sm"
-      padding="xl"
-      component="a"
-      href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-      target="_blank"
-    >
+    <Card shadow="sm" padding="xl" component="a" target="_blank">
       <Card.Section>
-        <Image src={restaurantData.foodImg} h={160} alt="No way!" />
+          <Image src={restaurantData.foodImg} h={160} alt="No way!" />
       </Card.Section>
 
       <Text fw={500} size="lg" mt="md">
         <span>{restaurantData.foods}</span>
         <br />
-         <span>
-          {restaurantData.collectTime.startTime} - {restaurantData.collectTime.endTime}
-        </span> 
+        <span>
+          {restaurantData.collectTime.startTime} -{" "}
+          {restaurantData.collectTime.endTime}
+        </span>
         <br />
-      
+
         <span>{restaurantData.rating}</span>
         <br />
         <span>{restaurantData.price}</span>
--- src/pages/RestaurantsPage.jsx
+++ src/pages/RestaurantsPage.jsx
@@ -1,18 +1,30 @@
 import GetRestaurants from "../components/GetRestaurants";
 import RestaurantCard from "../components/HomePage/RestaurantCard";
-import {useState , useEffect } from 'react'
- 
+import { useState, useEffect } from "react";
+import { NativeSelect } from "@mantine/core";
+import orderRestaurants from "../components/OrderRestaurantsBy";
+import FilterRestaurantsBy from "../components/FilterRestaurantsBy";
 
 const RestaurantsPage = () => {
+  const [restaurants, setRestaurants] = useState(null);
+  const [tempRestaurants, setTempRestaurants] = useState(restaurants);
 
-  const [restaurants, setRestaurants] = useState();
+  const [sortBy, setSortBy] = useState("Most Popular");
+  const [filterBy, setFilterBy] = useState("Pizza");
 
   useEffect(() => {
     const getRestaurants = async () => {
       try {
-          const restaurantsData = await GetRestaurants();
-          setRestaurants(restaurantsData);
-         
+        const restaurantsData = await GetRestaurants();
+
+        setRestaurants(restaurantsData);
+
+        const sortedRestaurants = orderRestaurants(restaurantsData, sortBy);
+        const filteredRestaurants = FilterRestaurantsBy(
+          sortedRestaurants,
+          filterBy
+        );
+        setTempRestaurants(filteredRestaurants);
       } catch (error) {
         console.log(error, "on getting restaurants");
       }
@@ -21,12 +33,36 @@ const RestaurantsPage = () => {
     getRestaurants();
   }, []);
 
+  useEffect(() => {
+    if (restaurants) {
+      const sortedRestaurants = orderRestaurants(restaurants, sortBy);
+      const filteredRestaurants = FilterRestaurantsBy(
+        sortedRestaurants,
+        filterBy
+      );
+      setTempRestaurants(filteredRestaurants);
+    }
+  }, [sortBy, filterBy]);
+
   return (
     <>
       <h1>Restaurants</h1>
-      {restaurants && restaurants.map((restaurant)=>
-      <RestaurantCard key={restaurant.id} restaurantData={restaurant}/>
-    )}
+      <NativeSelect
+        value={sortBy}
+        label="Sort by"
+        onChange={(event) => setSortBy(event.currentTarget.value)}
+        data={["Most Popular", "Price", "Distance"]}
+      />
+      <NativeSelect
+        value={filterBy}
+        label="Filter by"
+        onChange={(event) => setFilterBy(event.currentTarget.value)}
+        data={["Pizza", "Burger", "Noodles"]}
+      />
+      {tempRestaurants &&
+        tempRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} restaurantData={restaurant} />
+        ))}
     </>
   );
 };
