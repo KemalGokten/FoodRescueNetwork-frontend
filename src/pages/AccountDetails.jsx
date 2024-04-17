7863e29607b321cc3710f719cffd43eb209b39bf
Saeideh Bahadoran <saeideh.bahadoran@gmail.com>
2024-04-18 00:04:48 +0200

    add style to header/account setting page

--- src/pages/AccountDetails.jsx
+++ src/pages/AccountDetails.jsx
@@ -1,13 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext.jsx";
-import { Button, Group, PasswordInput, TextInput } from "@mantine/core";
+import { Button, Group, PasswordInput, TextInput, Flex } from "@mantine/core";
 import { useForm } from "@mantine/form";
 
 const AccountDetails = () => {
   const { user, setUser } = useContext(AuthContext);
   const [tempUser, setTempUser] = useState(user);
-
-
 
   const updateUser = async (e) => {
     e.preventDefault();
@@ -23,59 +21,58 @@ const AccountDetails = () => {
         }
       );
       if (response.ok) {
-        
         setUser(tempUser);
-        console.log("User successfully updated")
+        console.log("User successfully updated");
       }
     } catch (error) {
-      console.log(" Error by updating the user " , error);
+      console.log(" Error by updating the user ", error);
     }
   };
 
   return (
-    <>
+    <Flex direction="column" justify="center" align="center">
       <form onSubmit={(e) => updateUser(e)}>
-        <TextInput
-          label="Your name"
-          placeholder="Your name"
-          value={tempUser.fullName}
-          onChange={(event) =>
-            setTempUser({ ...tempUser, fullName: event.target.value })
-          }
-          required
-        />
-        <TextInput
-          label="Email"
-          placeholder="Email"
-          mt="md"
-          required
-          onChange={(event) =>
-            setTempUser({ ...tempUser, email: event.target.value })
-          }
-          value={tempUser.email}
-        />
-        <TextInput
-          label="User name"
-          placeholder="User name"
-          mt="md"
-          value={tempUser.userName}
-          disabled
-        />
-        <PasswordInput
-          label="Your password"
-          mt="md"
-          placeholder="Password"
-          required
-          onChange={(event) =>
-            setTempUser({ ...tempUser, password: event.target.value })
-          }
-          value={tempUser.password}
-        />
-        <Group justify="flex-end" mt="md">
-          <Button type="submit">Save changes</Button>
-        </Group>
+        <Flex direction={"column"} gap={16} w={350}>
+          <TextInput
+            label="Your name"
+            placeholder="Your name"
+            mt={32}
+            value={tempUser.fullName}
+            onChange={(event) =>
+              setTempUser({ ...tempUser, fullName: event.target.value })
+            }
+            required
+          />
+          <TextInput
+            label="Email"
+            placeholder="Email"
+            required
+            onChange={(event) =>
+              setTempUser({ ...tempUser, email: event.target.value })
+            }
+            value={tempUser.email}
+          />
+          <TextInput
+            label="User name"
+            placeholder="User name"
+            value={tempUser.userName}
+            disabled
+          />
+          <PasswordInput
+            label="Your password"
+            placeholder="Password"
+            required
+            onChange={(event) =>
+              setTempUser({ ...tempUser, password: event.target.value })
+            }
+            value={tempUser.password}
+          />
+          <Group justify="flex-end">
+            <Button type="submit">Save changes</Button>
+          </Group>
+        </Flex>
       </form>
-    </>
+    </Flex>
   );
 };
 
