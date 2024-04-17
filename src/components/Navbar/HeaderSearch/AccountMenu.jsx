263726557caa170e185e47e4f1de5c4614e24355
KemalGokten <ukemalgokten@gmail.com>
2024-04-17 22:04:04 +0200

    Create Account menu jsx and css

---
+++ src/components/Navbar/HeaderSearch/AccountMenu.jsx
@@ -0,0 +1,98 @@
+import cx from "clsx";
+import { useContext, useState } from "react";
+import {
+  Avatar,
+  UnstyledButton,
+  Menu,
+  rem,
+} from "@mantine/core";
+import {
+  IconLogout,
+  IconSettings,
+  IconSwitchHorizontal,
+} from "@tabler/icons-react";
+import classes from "./AccountMenu.module.css";
+import { RiUser3Line } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthContext";
+
+
+export default function AccountMenu() {
+  const [userMenuOpened, setUserMenuOpened] = useState(false);
+  const navigate = useNavigate();
+
+  const { logout } = useContext(AuthContext);
+
+  const handleClickDetails = () => {
+    navigate("/account_details");
+  }
+
+  const handleClickOrders = () => {
+    navigate("/previous_orders");
+  }
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  }
+
+  return (
+    <Menu
+      transitionProps={{ transition: "pop-top-right" }}
+      onClose={() => setUserMenuOpened(false)}
+      onOpen={() => setUserMenuOpened(true)}
+      withinPortal
+      trigger="hover"
+      openDelay={100}
+      closeDelay={100}
+    >
+      <Menu.Target>
+        <UnstyledButton
+          className={cx(classes.user, { [classes.userActive]: userMenuOpened })}
+        >
+
+          <Avatar color="blue" radius="sm"  size={40}>
+          <RiUser3Line size="1.5rem" />
+        </Avatar>
+
+        </UnstyledButton>
+      </Menu.Target>
+      <Menu.Dropdown>
+        <Menu.Item
+          leftSection={
+            <IconSettings
+              style={{ width: rem(16), height: rem(16) }}
+              stroke={1.5}
+            />
+          }
+          onClick={handleClickDetails}
+        >
+          Account settings
+        </Menu.Item>
+        <Menu.Item
+          leftSection={
+            <IconSwitchHorizontal
+              style={{ width: rem(16), height: rem(16) }}
+              stroke={1.5}
+            />
+          }
+          onClick={handleClickOrders}
+        >
+          Orders
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item
+          leftSection={
+            <IconLogout
+              style={{ width: rem(16), height: rem(16) }}
+              stroke={1.5}
+            />
+          }
+          onClick={handleLogout}
+        >
+          Logout
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
+}
